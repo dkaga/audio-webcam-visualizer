8d166c24c4da8def1b0b1958743a8d4bcab22c8b
dkaga <daniel.kaga@unity3d.com>
2022-11-16 16:10:31 -0800

    Add ThreeJS to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [glsl()],
+  build: {
+    rollupOptions: {
+      external: [
+        "three"
+      ],
+    },
+  },
   base: '/audio-webcam-visualizer/'
 });
